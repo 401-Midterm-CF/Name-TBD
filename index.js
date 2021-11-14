b6825e7f9e015e60e472a8ea3f633760015e99a1
Anthony Louis Morton <anthonymorton760@gmail.com>
2021-11-13 16:44:34 -0800

    added mongoose connection (see changelogs)

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 require('dotenv').config();
 const app = require('./lib/server.js');
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI)
 
 app.start(PORT)
